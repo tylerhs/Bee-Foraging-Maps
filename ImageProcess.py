3aca82724678a28d100dbcffaedacb839026041d
tylerhs <tylerhs.01@gmail.com>
2015-06-03 11:21:32 -0700

    Added EdgeDetection to ImageProcess

--- ImageProcess.py
+++ ImageProcess.py
@@ -1,3 +1,4 @@
+import ImageFilter
 from PIL import Image
 from PIL import ImageStat
 from colorsys import * 
@@ -61,4 +62,23 @@ def findYellow(imageName):
     #print(portion)
     return portion
       
+def countEdgePixels(file):
+    ''' counts the number of pixels that make up the edges of features'''
+    # define threshold for edges
+    threshold = 150 
     
+    # open image and filter
+    im = Image.open(file)
+    im2 = im.filter(ImageFilter.FIND_EDGES)
+    im2.save("Filtered.jpg")
+    im2 = im2.convert("L")
+	
+    # load pixels and count edge pixels
+    pix = im2.load()
+    pixels = 0
+    for x in range(0,im.size[0]):
+        for y in range(0, im.size[1]):
+            if pix[x,y] > threshold:
+                pixels += 1
+
+    return float(pixels) / (im.size[0]*im.size[1])
