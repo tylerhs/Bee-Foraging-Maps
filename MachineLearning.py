ebd07205540446cdc655ac03628619cd83b61e8b
cburgess@hmc.edu <cburgess@hmc.edu>
2015-06-10 13:13:28 -0700

    Fixed map stuff

--- MachineLearning.py
+++ MachineLearning.py
@@ -6,6 +6,7 @@ import matplotlib
 import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
 def svrAlg(): 
     X = [[0, 0], [2, 2]] #X is the array of metrics 
@@ -26,36 +27,59 @@ def densMap(fit, metricArray, n, overlap, imageSize ):
     height = imageSize[1] 
     overlapSize = int(overlap*n)
     rowTiles = int((width-n)/(overlapSize)+1)
-    colTiles = int(((height-n)/(overlapSize))+1)
+    #colTiles = int(((height-n)/(overlapSize))+1)
     
     
-    newArray = numpy.zeros((int((width-n)/(overlapSize)+1),int(((height-n)/(overlapSize))+1)))
+ #   newArray = numpy.zeros((int((width-n)/(overlapSize)+1),int(((height-n)/(overlapSize))+1)))
+ 
+    #newArray = numpy.zeros(width, height)
     #print newArray
+    points = []
     for i in range(len(densities)): 
-        newArray[i%(rowTiles),i/(rowTiles)] = densities[i]
-        
-    x = numpy.arange(0, rowTiles, 1)
-    y = numpy.arange(0, colTiles, 1)
-    X, Y = numpy.meshgrid(y,x)
+       # newArray[i%(rowTiles),i/(rowTiles)] = densities[i]
+      # newArray[(i%(rowTiles)+1)*(n/2) , (i/(rowTiles)+1)*(n/2)] = densities[i]
+       points += [[(i%(rowTiles)+1)*(n/2), (i/(rowTiles)+1)*(n/2)]]
+         
+         
+         
+    grid_x, grid_y = numpy.mgrid[0:width, 0:height]  #create a meshgrid  
+    
+    print grid_x
+    print grid_y
+    
+    #points = [] #create a list of where the points are 
+    
+    #values in the example is densities 
+    
+    
+    data = griddata(points, densities, (grid_x, grid_y), method = 'cubic') #interpolate to find all points 
+    
+    x = numpy.arange(0, width, 1)
+    y = numpy.arange(0, height, 1)
+    X, Y = numpy.meshgrid(x,y)
     
     ##Plotting 
     
-    print newArray
-    plt.figure()
-    fig = plt.contourf(X,Y, newArray)
+   # print newArray
+   # plt.figure()
+    plt.figure(figsize = (width, height))
+    fig = plt.contourf( data)
+    
     plt.clabel(fig, inline=1, fontsize=10)
     plt.title('New plot!!!')
+    
+ #   plt.imshow(plt.contourf(data))
     
     plt.savefig('ContourPlot.jpg')
     
 def overlayMap(mapName, contourName): 
     mapIm = Image.open(mapName) 
     contour = Image.open(contourName)
-    contour.convert('RGBA')
-    contour.putalpha(70)
+  #  contour.convert('RGBA')
+   # contour.putalpha(30)
     plt.imshow(contour)
     mapIm.convert('RGBA')
-    mapIm.putalpha(90)
+    mapIm.putalpha(150)
     plt.imshow(mapIm)
     
-    #contour.show()
+    plt.savefig('OverlayMap.jpg')
